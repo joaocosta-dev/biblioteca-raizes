6aa7b6207f41b37ed2f3904614f9447c377c82e1
Heitor-Bello <heitor.augustob@hotmail.com>
2024-10-29 16:46:12 -0300

    implementação da tela de registro/login e ajuste da navbar

---
+++ src/theme/themeNavbar.js
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#212121',
+    },
+    secondary: {
+      main: '#424242',
+    },
+  },
+});
