066cf3b3a37c1912f0837e3d8b0d9518b6f60635
joaocosta.dev <joaopedro-trabalho@outlook.com>
2024-10-30 12:04:25 -0300

    Ajustando o layout da página para inserção de livros

--- src/pages/CreateBook/CreateBook.js
+++ src/pages/CreateBook/CreateBook.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { useInsertDocument } from "../../hooks/useInsertDocument";
 import { useNavigate } from "react-router-dom";
 import { useAuthValue } from "../../contexts/AuthContext";
+import { TextField, Button, Typography, Box } from '@mui/material';
 
 const CreateBook = () => {
   const [title, setTitle] = useState("");
@@ -59,85 +60,233 @@ const CreateBook = () => {
   };
 
   return (
-    <div className={styles.create_book}>
-      <h2>Criar livro</h2>
-      <p>Adicione um novo livro à sua biblioteca!</p>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <span>Título:</span>
-          <input
-            type="text"
-            name="title"
+
+    <div className="flex justify-center">
+
+      <Box sx={{ m: 3, width: "100%", background: "linear-gradient(45deg, #0B8C7C, #086A5D);", borderRadius: "15px", padding: "20px", display: "flex", justifyContent: "space-between", boxShadow: "0 4px 10px rgba(0, 0, 0, 0.4)" }}>
+        <Box sx={{ m: 4, width: "65ch", display: "flex", justifyContent: "center", flexDirection: 'column' }}>
+          <Typography variant="h4" sx={{ color: '#FFFFFF' }}>Enriqueça sua biblioteca adicionando os seus livros disponiveis para locação</Typography>
+          <ul>
+            <p className="text-xl mt-14 mb-4 text-white font-bold">Observações para inserção:</p>
+            <li className="list-disc text-lg text-white ml-5 mb-2">Todos os campos devem ser preenchidos para conseguir criar um livro.</li>
+            <li className="list-disc text-lg text-white ml-5 mb-2">A URL da capa deve ser uma imagem cheia do livro.</li>
+            <li className="list-disc text-lg text-white ml-5 mb-2">As tags devem ser inseridas com o formato <code>#tag</code> e separadas por vírgula caso tenha mais de uma.</li>
+            <li className="list-disc text-lg text-white ml-5 mb-2">As categorias dos livros serão criadas de acordo com as tags. Exemplo de inserção: <code>#Autocuidado, #Estudos</code>.</li>
+          </ul>
+        </Box>
+        <Box component="form" onSubmit={handleSubmit} sx={{ m: 4, width: "60ch", background: "linear-gradient(225deg, #000000, #333333)", boxShadow: "0 4px 10px rgba(0, 0, 0, 0.4)", borderRadius: "15px", padding: "20px" }}>
+          <Typography variant="h4" sx={{ color: '#FFFFFF' }}>Criar livro</Typography>
+          <Typography variant="body1" sx={{ color: '#FFFFFF' }}>Adicione um novo livro à sua biblioteca!</Typography>
+
+          <TextField
+            label="Título"
+            variant="outlined"
             required
+            fullWidth
+            margin="normal"
             placeholder="Título do livro"
             onChange={(e) => setTitle(e.target.value)}
             value={title}
+            sx={{
+              '& .MuiOutlinedInput-root': {
+                '& fieldset': {
+                  borderColor: '#FFFFFF', // Cor da borda
+                },
+                '&:hover fieldset': {
+                  borderColor: '#FFFFFF', // Cor da borda ao passar o mouse
+                },
+                '&.Mui-focused fieldset': {
+                  borderColor: '#FFFFFF', // Cor da borda ao focar
+                },
+                color: '#FFFFFF', // Cor do texto
+                backgroundColor: 'transparent', // Fundo transparente
+              },
+              '& .MuiInputLabel-root': {
+                color: '#FFFFFF', // Cor do label
+                '&.Mui-focused': {
+                  color: '#FFFFFF', // Cor do label ao focar
+                },
+              },
+            }}
           />
-        </label>
-        <label>
-          <span>Autor:</span>
-          <input
-            type="text"
-            name="author"
+          <TextField
+            label="Autor"
+            variant="outlined"
             required
+            fullWidth
+            margin="normal"
             placeholder="Autor do livro"
             onChange={(e) => setAuthor(e.target.value)}
             value={author}
+            sx={{
+              '& .MuiOutlinedInput-root': {
+                '& fieldset': {
+                  borderColor: '#FFFFFF',
+                },
+                '&:hover fieldset': {
+                  borderColor: '#FFFFFF',
+                },
+                '&.Mui-focused fieldset': {
+                  borderColor: '#FFFFFF',
+                },
+                color: '#FFFFFF',
+                backgroundColor: 'transparent',
+              },
+              '& .MuiInputLabel-root': {
+                color: '#FFFFFF',
+                '&.Mui-focused': {
+                  color: '#FFFFFF',
+                },
+              },
+            }}
           />
-        </label>
-        <label>
-          <span>Descrição:</span>
-          <textarea
-            name="description"
+          <TextField
+            label="Descrição"
+            variant="outlined"
             required
+            fullWidth
+            margin="normal"
             placeholder="Descrição do livro"
+            multiline
+            rows={4}
             onChange={(e) => setDescription(e.target.value)}
             value={description}
-          ></textarea>
-        </label>
-        <label>
-          <span>URL da capa:</span>
-          <input
-            type="text"
-            name="image"
+            sx={{
+              '& .MuiOutlinedInput-root': {
+                '& fieldset': {
+                  borderColor: '#FFFFFF',
+                },
+                '&:hover fieldset': {
+                  borderColor: '#FFFFFF',
+                },
+                '&.Mui-focused fieldset': {
+                  borderColor: '#FFFFFF',
+                },
+                color: '#FFFFFF',
+                backgroundColor: 'transparent',
+              },
+              '& .MuiInputLabel-root': {
+                color: '#FFFFFF',
+                '&.Mui-focused': {
+                  color: '#FFFFFF',
+                },
+              },
+            }}
+          />
+          <TextField
+            label="URL da capa"
+            variant="outlined"
             required
+            fullWidth
+            margin="normal"
             placeholder="URL da capa do livro"
             onChange={(e) => setImage(e.target.value)}
             value={image}
+            sx={{
+              '& .MuiOutlinedInput-root': {
+                '& fieldset': {
+                  borderColor: '#FFFFFF',
+                },
+                '&:hover fieldset': {
+                  borderColor: '#FFFFFF',
+                },
+                '&.Mui-focused fieldset': {
+                  borderColor: '#FFFFFF',
+                },
+                color: '#FFFFFF',
+                backgroundColor: 'transparent',
+              },
+              '& .MuiInputLabel-root': {
+                color: '#FFFFFF',
+                '&.Mui-focused': {
+                  color: '#FFFFFF',
+                },
+              },
+            }}
           />
-        </label>
-        <label>
-          <span>Tags:</span>
-          <input
-            type="text"
-            name="tags"
+          <TextField
+            label="Tags"
+            variant="outlined"
             required
+            fullWidth
+            margin="normal"
             placeholder="Tags separadas por vírgula"
             onChange={(e) => setTags(e.target.value)}
             value={tags}
+            sx={{
+              '& .MuiOutlinedInput-root': {
+                '& fieldset': {
+                  borderColor: '#FFFFFF',
+                },
+                '&:hover fieldset': {
+                  borderColor: '#FFFFFF',
+                },
+                '&.Mui-focused fieldset': {
+                  borderColor: '#FFFFFF',
+                },
+                color: '#FFFFFF',
+                backgroundColor: 'transparent',
+              },
+              '& .MuiInputLabel-root': {
+                color: '#FFFFFF',
+                '&.Mui-focused': {
+                  color: '#FFFFFF',
+                },
+              },
+            }}
           />
-        </label>
-        <label>
-          <span>Páginas:</span>
-          <input
-            type="number"
-            name="pages"
+          <TextField
+            label="Páginas"
+            variant="outlined"
             required
-            placeholder="Número de págins do livro"
+            fullWidth
+            margin="normal"
+            placeholder="Número de páginas do livro"
+            type="number"
             onChange={(e) => setPages(e.target.value)}
             value={pages}
+            sx={{
+              '& .MuiOutlinedInput-root': {
+                '& fieldset': {
+                  borderColor: '#FFFFFF',
+                },
+                '&:hover fieldset': {
+                  borderColor: '#FFFFFF',
+                },
+                '&.Mui-focused fieldset': {
+                  borderColor: '#FFFFFF',
+                },
+                color: '#FFFFFF',
+                backgroundColor: 'transparent',
+              },
+              '& .MuiInputLabel-root': {
+                color: '#FFFFFF',
+                '&.Mui-focused': {
+                  color: '#FFFFFF',
+                },
+              },
+            }}
           />
-        </label>
-        {!response.loading && <button className="btn">Criar livro!</button>}
-        {response.loading && (
-          <button className="btn" disabled>
-            Aguarde...
-          </button>
-        )}
-        {(response.error || formError) && (
-          <p className="error">{response.error || formError}</p>
-        )}
-      </form>
+          <div className="flex justify-end">
+            <Button
+              variant="contained"
+              color="primary"
+              type="submit"
+              sx={{ mt: 2,fontSize:'18px',padding:'12px', color: '#FFF', background: '#0B8C7C', ":hover": { background: '#086A5D' } }} // Cor do botão
+              disabled={response.loading}
+            >
+              {response.loading ? 'Aguarde...' : 'Criar livro'}
+            </Button>
+          </div>
+
+
+          {(response.error || formError) && (
+            <Typography color="error" sx={{ mt: 2 }}>
+              {response.error || formError}
+            </Typography>
+          )}
+        </Box>
+      </Box>
     </div>
   );
 };
